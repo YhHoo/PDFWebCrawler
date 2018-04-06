ae7b930a99106b3a2c3a399bca26dc9e48d87ca0
Hoo Yu Heng <hooyuheng@gmail.com>
2018-04-06 20:42:48 +0800

    new error found

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import requests
 import re
 import codecs
+from pandas import DataFrame, read_csv
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
 from urllib.request import urlopen
 import bibtexparser
 
+# l = ['asd', 'sda', 'shd']
+# df = DataFrame(data=l)
+# print(df)
 
-with codecs.open('C://Users//YH//Desktop//scopus.bib', 'r', errors='replace', encoding='utf-8') as bib_file:
-    count = 1
-    for line in bib_file:
-        if line[0:3] == 'doi':
-            doi = re.sub('[{},]', '', line[4:])
-            print(count, doi)
-            count += 1
+# with codecs.open('C://Users//YH//Desktop//scopus.bib', 'r', errors='replace', encoding='utf-8') as bib_file:
+#     doi_list = []
+#     for line in bib_file:
+#         if line[0:3] == 'doi':
+#             doi = re.sub('[{},]', '', line[4:])
+#             doi_list.append(doi.strip())
+#
+# df = DataFrame(data=doi_list)
+# df.to_csv('DOI_List_p1.csv')
 
 
 # return redirected address if they redirect
@@ -31,10 +37,9 @@ def check_redirect(url_to_go):
         return None
 
 
-def search_save_pdf(doi, save_dir):
+def search_save_pdf(doi, save_dir=None):
     # From DOI to redirected website to crawl the sci direct url
     doi_head = 'http://doi.org/'
-    doi = '10.1016/j.watres.2018.01.037'
     url = doi_head + doi
     print('CRAWLING >>> [{}]'.format(url))
     response_1 = requests.get(url)
@@ -48,6 +53,7 @@ def search_save_pdf(doi, save_dir):
     # from the sci direct url crawl the pdf file
     response_2 = requests.get(sci_dir_url)
     response_in_text = response_2.text
+    print(response_in_text)
     soup_2 = BeautifulSoup(response_in_text, 'lxml')
     tag = soup_2.find('meta', attrs={'name': 'citation_pdf_url'})
     pdf_url = tag['content']
@@ -59,12 +65,19 @@ def search_save_pdf(doi, save_dir):
     soup_3 = BeautifulSoup(response_in_text, 'lxml')
     tag = soup_3.find('a')
     pdf_url_secure = tag['href']
-    print('SAVING >>> [{}]'.format(pdf_url_secure))
+    print('SAVING >>> [{}]\n'.format(pdf_url_secure))
 
-    # access into pdf and save
-    response_3 = requests.get(pdf_url_secure)
-    # save to local disk
-    with open('PDF_1.pdf', 'wb') as f:
-        f.write(response_3.content)
+    # # access into pdf and save
+    # response_3 = requests.get(pdf_url_secure)
+    # # save to local disk
+    # with open('PDF_1.pdf', 'wb') as f:
+    #     f.write(response_3.content)
 
 
+# Do the extraction
+# df = read_csv('DOI_List_p1.csv', index_col=0)
+# df = df.values
+# for doi in df:
+#     search_save_pdf(doi=doi[0])
+
+search_save_pdf(doi='10.1016/j.jclepro.2018.01.239')
